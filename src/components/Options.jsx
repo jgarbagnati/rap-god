c44ba2c26bbfd5a9625f8e3222b9a29bed6e809e
CarpeSwag <james@garbagnati.com>
2017-12-08 03:32:46 -0800

    Simple, unstyled lyrics generator.

---
+++ src/components/Options.jsx
@@ -0,0 +1,16 @@
+import Inferno from 'inferno';
+import Component from 'inferno-component';
+
+export default class Boteezy extends Component {
+	constructor(props) {
+		super(props);
+	}
+	
+	render() {
+		return (
+			<div id='option-panel'>
+				
+			</div>
+		);
+	}
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,83 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
+import Options from './components/Options';
+import Tracery from './lib/tracery';
+import Rules from './lib/rules';
 
 export default class Boteezy extends Component {
 	constructor(props) {
 		super(props);
 		
-		this.state = {}
+		let grammars = {};
+		let options = [];
+		let defaultOption = '';
+		let rule = '';
+		for (rule in Rules) {
+			if (rule === 'defaultOption') {
+				defaultOption = Rules[rule];
+				console.log(defaultOption);
+			} else {
+				options.push(rule);
+				grammars[rule] = Tracery.createGrammar(Rules[rule]);
+			}
+		}
+		
+		this.state = {
+			blurbs: [],
+			postCount: 0,
+			options: options,
+			grammars: grammars,
+			currOption: defaultOption
+		};
+		
+		this.selectGrammar = this.selectGrammar.bind(this);
+		this.generateLyrics = this.generateLyrics.bind(this);
+		this.displayLyrics = this.displayLyrics.bind(this);
+	}
+	
+	selectGrammar(grammar) {
+		this.setState({
+			currOption: grammar
+		});
+	}
+	
+	generateLyrics(evt) {
+		let grammar = this.state.grammars[this.state.currOption];
+		let generated = grammar.flatten('#origin#');
+		this.state.blurbs.push(generated);
+		
+		this.setState({
+			postCount: this.state.postCount + 1
+		});
+	}
+	
+	displayLyrics() {
+		let out = [];
+		let i = this.state.blurbs.length;
+		while (i --> 0) {
+			let blurb = (
+				<div key={i}
+					 className='blurb'>
+					{this.state.blurbs[i]}
+				</div>
+			);
+			
+			out.push(blurb);
+		}
+		return out;
 	}
 	
 	render() {
 		return (
 			<div id='inferno-root'>
+				<div className='lyricBlock'>
+					<div onclick={this.generateLyrics}>
+						<h1> lol </h1>
+						{this.displayLyrics()}
+					</div>
+				</div>
+				<Options selectGrammar={this.selectGrammar}
+					options={this.state.options} />
 			</div>
 		);
 	}
