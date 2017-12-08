80a3a35b6ec0515a041b239e61f1bcd0b76000b2
CarpeSwag <james@garbagnati.com>
2017-12-08 03:42:07 -0800

    Added simple post-processing from P5.

--- src/index.js
+++ src/index.js
@@ -43,12 +43,58 @@ export default class Boteezy extends Component {
 	
 	generateLyrics(evt) {
 		let grammar = this.state.grammars[this.state.currOption];
-		let generated = grammar.flatten('#origin#');
+		let generated = this.generate(grammar);
 		this.state.blurbs.push(generated);
 		
 		this.setState({
 			postCount: this.state.postCount + 1
 		});
+	}
+	
+	generate(grammar) {
+		let gen = '';
+		do {
+			gen = this.postProcess(grammar.flatten('#origin#'))
+		} while (gen.length == 0);
+		return gen;
+	}
+
+	// Post processing
+	postProcess(input) {
+		return this.processSizeLimit(input);
+	}
+
+	processSizeLimit(input) {
+		let temp = "";
+		let split = input.split('.');
+		for (let i = 0; i < split.length; ++i) {
+			let str = split[i];
+			if (this.checkLength(str + '.')) {
+				return str + '.';
+			} else if (this.checkLength(temp + str + '.')) {
+				return temp + str + '.';
+			} else if (this.checkLowerBound(str + '.') || this.checkLowerBound(temp + str + '.')) {
+				temp += str + '.';
+			} else {
+				temp = "";
+			}
+		}
+		return "";
+	}
+
+	// Check lower bound
+	checkLowerBound(str) {
+		return !(str.length < 60);
+	}
+
+	// Check upper bound
+	checkUpperBound(str) {
+		return !(str.length > 140);
+	}
+
+	// Check if we're between 100 and 140 characters.
+	checkLength(str) {
+		return this.checkLowerBound(str) && this.checkUpperBound(str);
 	}
 	
 	displayLyrics() {
@@ -72,9 +118,9 @@ export default class Boteezy extends Component {
 			<div id='inferno-root'>
 				<div className='lyricBlock'>
 					<div onclick={this.generateLyrics}>
-						<h1> lol </h1>
-						{this.displayLyrics()}
+						<h1> Generate </h1>
 					</div>
+					{this.displayLyrics()}
 				</div>
 				<Options selectGrammar={this.selectGrammar}
 					options={this.state.options} />
